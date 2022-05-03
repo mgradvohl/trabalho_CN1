07ddc3e8169f7107e1346eb98fa03c3045fbe259
Marina Gradvohl <marinagradvohl@gmail.com>
2022-05-03 14:45:20 -0300

    começa implementação da conversão octal

--- ep1.c
+++ ep1.c
@@ -10,6 +10,7 @@ MARINA BRASIL
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void conversao(double num);
 void sistemaLinear();
@@ -67,7 +68,7 @@ int main () {
 
 void conversao(double num) {
 
-    // -------------------------- CONVERSAO BINARIA -----------------------------------//
+    // --------------------------- CONVERSAO BINARIA --------------------------------//
 
     // vetor para salvar o numero binario
     int numBinarioInt[32]; // parte inteira do numero binario
@@ -77,8 +78,9 @@ void conversao(double num) {
     // printf("%d", n);
     double m = num - n;
     // printf("%lf", m);
+
     // conversao da parte inteira
-    int i = 0, k = 0;
+    int i = 0; // armazena qtd de casas da parte inteira
     while (n > 0) {
         numBinarioInt[i] = n % 2; // salva resto da divisao por 2
         n = n / 2; // salva o resultado inteiro da divisao por 2
@@ -91,13 +93,13 @@ void conversao(double num) {
         printf("%d", numBinarioInt[j]);
 
     // testa se existe parte fracionaria antes de fazer a conversao
+    int k = 0; // armazena qtd de casas da parte fracionaria
     if (m) {
         // conversao da parte fracionaria
         while (m > 0) {
             numBinarioFrac[k] = (int)(m *= 2); // salva a parte inteira da multiplicacao
             m = m - (int)m; // salva somente a parte fracionaria apos multiplicacao
-            k++; // aumenta contador para impressao ao final
-            
+            k++; // aumenta contador para impressao ao final 
         };
         printf(".");
         // imprime a parte fracionaria em ordem direta
@@ -108,12 +110,27 @@ void conversao(double num) {
 
     // -------------------------- CONVERSAO OCTAL -----------------------------------//
 
+    // vetor para salvar o numero octal
+    int numOctalInt[32]; // parte inteira do numero octal
+    int numOctalFrac[32]; // parte fracionaria do numero octal
 
+    double div_int  = ceil(i/3.0); // encontra quantos conjuntos de 3 existem no inteiro
+    double div_frac = ceil(k/3.0); // encontra quantos conjuntos de 3 existem na fracao
 
+    // fazer for pra pegar de 3 em 3 e comparar com a tabela de conversao e armazenar no numOctalInt
+    // for(int j = div_int-1; j >= 0; j--) {
+    //
+    // }
+
+    // fazer outro for para imprimir o numOctalInt
+
+    // fazer a mesma logica pra parte fracionaria
 
     // ----------------------- CONVERSAO HEXADECIMAL --------------------------------//
 
+    // usar a mesma logica da conversao octal, mas separar de 4 em 4
 
+    
 };
 
 void sistemaLinear() {
